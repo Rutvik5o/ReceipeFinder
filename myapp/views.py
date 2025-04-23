30a4a48b9760ba33fb6ff2e4f45e9cd7e0381af5
Rutvik5o <prajapatirutvik987@gmail.com>
2025-04-23 11:05:39 +0530

    Completed showing receipes

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def indexpage(request):
+    url = requests.get("https://dummyjson.com/recipes")
+    response = url.json()
+    data = response['recipes']
+    print(data)
+
+    context ={
+
+        "data":data
+
+    }
+
+    return render(request,"index.html",context)
